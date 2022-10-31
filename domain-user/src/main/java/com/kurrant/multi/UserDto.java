a80c969c654a61c8b471b6a2c4045219791b1172
“sangjin <sangjin.lee@dalicious.co>
2022-10-31 13:17:56 +0900

    백업용

--- domain-user/src/main/java/com/kurrant/multi/UserDto.java
+++ domain-user/src/main/java/com/kurrant/multi/UserDto.java
@@ -1,7 +1,10 @@
 package com.kurrant.multi;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class UserDto {
     public Integer supplierCorpId;
 
@@ -24,4 +27,18 @@ public class UserDto {
 //    public LocalDateTime created;
 
     public Integer box;
+
+    UserDto(User user){
+        this.supplierCorpId = user.getSupplierCorpId();
+        this.customerCorpId = user.getCustomerCorpId();
+        this.type = user.getType();
+        this.level = user.getLevel();
+        this.locationId = user.getLocationId();
+        this.registerType = user.getRegisterType();
+        this.gender = user.getGender();
+        this.verifyStatus = user.getVerifyStatus();
+        this.status = user.getStatus();
+        this.box = user.getBox();
+    }
+
 }
